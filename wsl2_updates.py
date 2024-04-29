da0cca194885b730ab130aa287acc151d3d5679b
nebulousanchor <ike.trinity@gmail.com>
2024-04-28 19:29:40 -0700

    Script to for configuration of systems that use WSL2

--- wsl2_updates.py
+++ wsl2_updates.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 import os
-import sys
 import subprocess
 
 # Update the environment variable
-os.environ['VAGRANT_WSL_ENABLE_WINDOWS_ACCESS'] = '1'
+os.environ['VAGRANT_WSL_ENABLE_WINDOWS_ACCESS'] = 1
 
 # Verify the updated value
 print('VAGRANT_WSL_ENABLE_WINDOWS_ACCESS = ' + os.environ['VAGRANT_WSL_ENABLE_WINDOWS_ACCESS'])
 
+# Add the VirtualBox path to the PATH environment variable
 app_path = "/mnt/c/Program Files/Oracle/VirtualBox"
 os.environ["PATH"] += os.pathsep + app_path
 
-print ('Added VirtualBox path to PATH: ' + app_path)
+print ('Added VirtualBox path to PATH: ' + os.environ["PATH"])
 
 # Run the shell command
 process = subprocess.run('vagrant plugin install virtualbox_WSL2', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
